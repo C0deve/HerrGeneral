3bf762e8c212986d8ac412b514fe191e1cc497ee
Brice Chapellier <brice.chapellier@gold.fr>
2023-08-03 00:42:51 +0200

    Add comments

--- src/HerrGeneral.Contracts/WriteSide/ICommand.cs
+++ src/HerrGeneral.Contracts/WriteSide/ICommand.cs
@@ -2,10 +2,26 @@ using System.Text;
 
 namespace HerrGeneral.Contracts.WriteSide;
 
+/// <summary>
+/// Command interface (Write side)
+/// </summary>
+/// <typeparam name="TResult"></typeparam>
 public interface ICommand<out TResult>
 {
+    /// <summary>
+    /// Id of the command
+    /// </summary>
     Guid Id { get; }
+    
+    /// <summary>
+    /// Execution date of the command
+    /// </summary>
     DateTime ExecutionDate { get; }
 
+    /// <summary>
+    /// Used to control log format of the command
+    /// </summary>
+    /// <param name="sb"></param>
+    /// <returns></returns>
     StringBuilder Log(StringBuilder sb);
 }
--- src/HerrGeneral.Contracts/WriteSide/IEvent.cs
+++ src/HerrGeneral.Contracts/WriteSide/IEvent.cs
@@ -2,15 +2,32 @@ using System.Text;
 
 namespace HerrGeneral.Contracts.WriteSide;
 
+/// <summary>
+/// Event interface (Write side)
+/// </summary>
 public interface IEvent
 {
+    /// <summary>
+    /// Date of the event
+    /// </summary>
     DateTime DateTimeEventOccurred { get; }
+    
+    /// <summary>
+    /// Id of the event
+    /// </summary>
     Guid EventId { get; }
+    
     /// <summary>
     /// Id of the command at the origin of the event
     /// </summary>
     Guid SourceCommandId { get; }
     
+    /// <summary>
+    /// Used to control log format of the event
+    /// </summary>
+    /// <param name="sb"></param>
+    /// <param name="indent"></param>
+    /// <returns></returns>
     StringBuilder Log(StringBuilder sb, string indent);
 }
 
